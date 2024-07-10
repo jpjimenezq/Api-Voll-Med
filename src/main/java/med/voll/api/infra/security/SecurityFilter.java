5b010ec9dcff45a3fe00aee74481f8838484b7aa
jpjimenezq <jpjimenez0206@gmail.com>
2024-07-10 01:34:26 -0500

    First commit

---
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -0,0 +1,30 @@
+package med.voll.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // Obtener el token del header
+        var token = request.getHeader("Authorization");
+        if (token != null){
+            token = token.replace("Bearer ", "");
+            System.out.println(token);
+            System.out.println(tokenService.getSubject(token));
+        }
+        filterChain.doFilter(request, response);
+    }
+}
